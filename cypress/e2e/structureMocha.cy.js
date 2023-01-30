18524e492b0ec8bbdefb50eac07e339a09c7285c
1000len-6890 <ameppotchuente@testo.de>
2023-01-30 21:19:39 +0100

    third commit

--- cypress/e2e/structureMocha.cy.js
+++ cypress/e2e/structureMocha.cy.js
@@ -3,11 +3,11 @@ describe('My test suite', function test(){
     it('test case 2', ()=>{
 
     })
-    it('test case 2',()=>{
-
+    it.skip('test case 2',()=>{
+        // skip: Indicates this test should not be executed.
     })
-    it('test case 2',()=>{
-        
+    it.only('test case 2',()=>{
+        //only: Indicates this test should be executed exclusively.
     })
 })
 describe('My test suite 2', function(){
